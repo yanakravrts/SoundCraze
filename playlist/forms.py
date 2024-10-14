26926998832a81daaf7e66f7568bd1476d089c10
Yana Kravets <yanakravets@MacBook-Pro-Yana.local>
2024-10-14 23:00:05 +0300

    language changed

--- playlist/forms.py
+++ playlist/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class PlaylistForm(forms.Form):
-    artist = forms.CharField(label='Автор', max_length=100)
-    genre = forms.CharField(label='Жанр', max_length=100, required=False)  # Поле для жанру, необов'язкове
-    number_of_songs = forms.IntegerField(label='Кількість пісень у плейлисті', min_value=1, max_value=50)
+    artist = forms.CharField(label='Artist', max_length=100)
+    genre = forms.CharField(label='Genre', max_length=100, required=False)  # Поле для жанру, необов'язкове
+    number_of_songs = forms.IntegerField(label='Number of songs', min_value=1, max_value=50)
